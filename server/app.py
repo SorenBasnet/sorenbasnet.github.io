e6bdf685adcb5636f19297eeb99ad3ae4617a51c
SorenBasnet <basnetsoren01@gmail.com>
2023-11-06 18:56:59 -0600

    trial3

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify 
+from flask_cors import CORS 
+
+app = Flask(__name__) 
+CORS(app) 
+
+@app.route("/jokes") 
+def send_jokes(): 
+    return jsonify("data") 
+
+@app.route("/jokes=<int:number>")
+def send_jokesNum(): 
+    return jsonify("jokes but numberred"); 
+
+if __name__ == "main": 
+    app.run() 

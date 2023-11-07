4e8897e294dc088e49023ae93fa06703fe3590fa
Soren <basnetsoren01@gmail.com>
2023-11-07 10:27:28 -0600

    f

--- server/app.py
+++ server/app.py
@@ -1,8 +1,6 @@
-import json
 import pyjokes
 from flask import Flask, jsonify
 from flask_cors import CORS
-from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
@@ -28,21 +26,6 @@ def send_jokess(cat, lang):
 def send_jokes():
     joke = pyjokes.get_joke("en", "chuck")
     return jsonify(joke)
-
-
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
-    # start with the correct headers and status code from the error
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
 
 
 if __name__ == "__main__":
